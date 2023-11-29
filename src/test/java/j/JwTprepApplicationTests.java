b8f63dcbc12209b3e48212f437d5681ea8777e88
Jon Eikholm <joneikholm@hotmail.com>
2023-11-29 10:52:02 +0100

    removed tests

--- src/test/java/j/JwTprepApplicationTests.java
+++ src/test/java/j/JwTprepApplicationTests.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,28 +20,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 //@AllArgsConstructor
 class JwTprepApplicationTests {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Test
-    void contextLoads() {
-        Object o = userRepository.findByUsername("Ole");
-        assertNotNull(o);
-    }
-
-    MockMvc mockMvc;
-
-    @BeforeEach
-    public void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(new JwtController()).build();
-    }
-
-    @Test
-    public void index() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())  // http 200 kode for "ok"
-                .andExpect(view().name("index"));
-    }
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @Test
+//    void contextLoads() {
+//        Object o = userRepository.findByUsername("Ole");
+//        assertNotNull(o);
+//    }
+//
+//    MockMvc mockMvc;
+//
+//    @BeforeEach
+//    public void setUp() throws Exception {
+//        mockMvc = MockMvcBuilders.standaloneSetup(new JwtController()).build();
+//    }
+//
+//    @Test
+//    public void index() throws Exception {
+//        mockMvc.perform(get("/"))
+//                .andExpect(status().isOk())  // http 200 kode for "ok"
+//                .andExpect(view().name("index"));
+//    }
 //
 //    @Test
 //    public void addUser() throws Exception {
