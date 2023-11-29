f2ef79fcca42a5d097dfda479e6f229efc39f4d1
Jon Eikholm <joneikholm@hotmail.com>
2023-11-29 14:20:48 +0100

    cross origin = *

--- src/main/java/j/security/config/SecurityConfiguration.java
+++ src/main/java/j/security/config/SecurityConfiguration.java
@@ -13,11 +13,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @AllArgsConstructor
+@CrossOrigin("*")
 public class SecurityConfiguration implements WebMvcConfigurer {
     private JwtAuthenticationEntryPoint authenticationEntryPoint;
     private JwtFilter filter;
